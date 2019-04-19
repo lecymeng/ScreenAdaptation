d45bde579c076dcb05b0b343ec61f5cfe8fb37a8
zhengwei.zhang <zhengwei.zhang@ihandysoft.com>
2019-04-19 14:24:43 +0800

    添加AutoLayout适配demo

---
+++ autolayout/src/main/java/me/weicools/autolayout/MainActivity.java
@@ -0,0 +1,13 @@
+package me.weicools.autolayout;
+
+import android.os.Bundle;
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity {
+
+  @Override
+  protected void onCreate (Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
