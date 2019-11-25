0fae26af62cd78a59df951dbdf769c65c6622b96
zhengwei.zhang <zhengwei.zhang@enerjoy.life>
2019-11-25 16:48:28 +0800

    set status bar

--- app/src/main/java/me/weicools/screen/NonAdaptActivity.java
+++ app/src/main/java/me/weicools/screen/NonAdaptActivity.java
@@ -2,6 +2,7 @@ package me.weicools.screen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NonAdaptActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class NonAdaptActivity extends AppCompatActivity {
   protected void onCreate (Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_non_adapt);
+
+    getWindow().getDecorView()
+        .setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
   }
 }
